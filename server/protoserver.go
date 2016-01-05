4bc6bff6ff5f3dfcbede3c3d01f0d2063a09a503
Peter Edge <peter.edge@gmail.com>
2016-01-05 16:59:47 +0100

    updates to proto/serve

--- server/protoserver.go
+++ server/protoserver.go
@@ -26,22 +26,13 @@ var (
 	ErrMustSpecifyPort = errors.New("must specify port")
 	// ErrMustSpecifyRegisterFunc is the error if no registerFunc is specifed to Serve.
 	ErrMustSpecifyRegisterFunc = errors.New("must specify registerFunc")
-	// ErrCannotSpecifyBothHTTPPortAndHTTPAddress is the error if both HTTPPort and HTTPAddress are specified in ServeOptions.
-	ErrCannotSpecifyBothHTTPPortAndHTTPAddress = errors.New("cannot specify both HTTPPort and HTTPAddress")
 )
 
 // ServeOptions represent optional fields for serving.
 type ServeOptions struct {
 	HTTPPort         uint16
-	DebugPort        uint16
-	Version          *protoversion.Version
 	HTTPRegisterFunc func(context.Context, *runtime.ServeMux, *grpc.ClientConn) error
-	// either HTTPPort or HTTPAddress can be set, but not both
-	HTTPAddress           string
-	HTTPListener          net.Listener
-	ServeMuxOptions       []runtime.ServeMuxOption
-	HTTPBeforeShutdown    func()
-	HTTPShutdownInitiated func()
+	Version          *protoversion.Version
 }
 
 // Serve serves stuff.
@@ -50,32 +41,13 @@ func Serve(
 	registerFunc func(*grpc.Server),
 	opts ServeOptions,
 ) (retErr error) {
-	start := time.Now()
-	defer func() {
-		if retErr != nil {
-			protolog.Error(
-				&ServerFinished{
-					Error:    retErr.Error(),
-					Duration: prototime.DurationToProto(time.Since(start)),
-				},
-			)
-		} else {
-			protolog.Info(
-				&ServerFinished{
-					Duration: prototime.DurationToProto(time.Since(start)),
-				},
-			)
-		}
-	}()
 	if port == 0 {
 		return ErrMustSpecifyPort
 	}
 	if registerFunc == nil {
 		return ErrMustSpecifyRegisterFunc
 	}
-	if opts.HTTPPort != 0 && opts.HTTPAddress != "" {
-		return ErrCannotSpecifyBothHTTPPortAndHTTPAddress
-	}
+	defer func(start time.Time) { logServerFinished(start, retErr) }(time.Now())
 	s := grpc.NewServer(grpc.MaxConcurrentStreams(math.MaxUint32))
 	registerFunc(s)
 	if opts.Version != nil {
@@ -86,30 +58,13 @@ func Serve(
 		return err
 	}
 	grpcErrC := make(chan error)
-	grpcDebugErrC := make(chan error)
 	httpErrC := make(chan error)
 	errCCount := 1
 	go func() { grpcErrC <- s.Serve(listener) }()
-	if opts.DebugPort != 0 {
-		errCCount++
-		debugServer := &graceful.Server{
-			Timeout: 1 * time.Second,
-			Server: &http.Server{
-				Addr:    fmt.Sprintf(":%d", opts.DebugPort),
-				Handler: http.DefaultServeMux,
-			},
-		}
-		go func() { grpcDebugErrC <- debugServer.ListenAndServe() }()
-	}
-	if (opts.HTTPPort != 0 || opts.HTTPAddress != "") && (opts.Version != nil || opts.HTTPRegisterFunc != nil) {
+	if opts.HTTPPort != 0 && (opts.Version != nil || opts.HTTPRegisterFunc != nil) {
 		time.Sleep(1 * time.Second)
 		ctx, cancel := context.WithCancel(context.Background())
-		var mux *runtime.ServeMux
-		if len(opts.ServeMuxOptions) == 0 {
-			mux = runtime.NewServeMux()
-		} else {
-			mux = runtime.NewServeMux(opts.ServeMuxOptions...)
-		}
+		mux := runtime.NewServeMux()
 		conn, err := grpc.Dial(fmt.Sprintf("0.0.0.0:%d", port), grpc.WithInsecure())
 		if err != nil {
 			glog.Flush()
@@ -137,40 +92,25 @@ func Serve(
 			}
 		}
 		httpAddress := fmt.Sprintf(":%d", opts.HTTPPort)
-		if opts.HTTPAddress != "" {
-			httpAddress = opts.HTTPAddress
-		}
 		httpServer := &http.Server{
 			Addr:    httpAddress,
 			Handler: mux,
 		}
 		gracefulServer := &graceful.Server{
-			Timeout:        1 * time.Second,
-			BeforeShutdown: opts.HTTPBeforeShutdown,
+			Timeout: 1 * time.Second,
 			ShutdownInitiated: func() {
 				glog.Flush()
 				cancel()
-				if opts.HTTPShutdownInitiated != nil {
-					opts.HTTPShutdownInitiated()
-				}
 			},
 			Server: httpServer,
 		}
 		errCCount++
-		go func() {
-			if opts.HTTPListener != nil {
-				httpErrC <- gracefulServer.Serve(opts.HTTPListener)
-			} else {
-				httpErrC <- gracefulServer.ListenAndServe()
-			}
-		}()
+		go func() { httpErrC <- gracefulServer.ListenAndServe() }()
 	}
 	protolog.Info(
 		&ServerStarted{
-			Port:        uint32(port),
-			HttpPort:    uint32(opts.HTTPPort),
-			DebugPort:   uint32(opts.DebugPort),
-			HttpAddress: opts.HTTPAddress,
+			Port:     uint32(port),
+			HttpPort: uint32(opts.HTTPPort),
 		},
 	)
 	var errs []error
@@ -182,15 +122,6 @@ func Serve(
 				errs = append(errs, fmt.Errorf("grpc error: %s", grpcErr.Error()))
 			}
 			grpcStopped = true
-		case grpcDebugErr := <-grpcDebugErrC:
-			if grpcDebugErr != nil {
-				errs = append(errs, fmt.Errorf("grpc debug error: %s", grpcDebugErr.Error()))
-			}
-			if !grpcStopped {
-				s.Stop()
-				_ = listener.Close()
-				grpcStopped = true
-			}
 		case httpErr := <-httpErrC:
 			if httpErr != nil {
 				errs = append(errs, fmt.Errorf("http error: %s", httpErr.Error()))
@@ -206,4 +137,21 @@ func Serve(
 		return fmt.Errorf("%v", errs)
 	}
 	return nil
+}
+
+func logServerFinished(start time.Time, err error) {
+	if err != nil {
+		protolog.Error(
+			&ServerFinished{
+				Error:    err.Error(),
+				Duration: prototime.DurationToProto(time.Since(start)),
+			},
+		)
+	} else {
+		protolog.Info(
+			&ServerFinished{
+				Duration: prototime.DurationToProto(time.Since(start)),
+			},
+		)
+	}
 }
