25db9bbbe59e82c1d3c6789f3b38901aba6c334e
Joe Doliner <jdoliner@gmail.com>
2016-06-27 18:42:37 -0700

    Run make proto.

--- http/protohttp.pb.go
+++ http/protohttp.pb.go
@@ -33,6 +33,8 @@ func init() {
 	proto.RegisterType((*BasicAuth)(nil), "protohttp.BasicAuth")
 }
 
+func init() { proto.RegisterFile("http/protohttp.proto", fileDescriptor0) }
+
 var fileDescriptor0 = []byte{
 	// 99 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0x12, 0xc9, 0x28, 0x29, 0x29,
